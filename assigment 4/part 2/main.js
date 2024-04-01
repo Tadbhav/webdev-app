21bbb54bebb2b255c6a86eca9a383d308419a463
Tadbhav <tadbhav.marken@dcmail.ca>
2024-04-01 18:18:57 -0400

    wiring up

--- assigment 4/part 2/main.js
+++ assigment 4/part 2/main.js
@@ -27,3 +27,18 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', (e) => {
+    const btnClass = btn.getAttribute("class");
+    if (btnClass === 'dark')
+    {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    }
+    else if (btnClass === 'light')
+    {   
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
