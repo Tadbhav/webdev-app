c71f7a695ecbd96fe660cc5e45e1b6e393af8cff
Tadbhav <tadbhav.marken@dcmail.ca>
2024-04-01 19:19:28 -0400

    evil circle added

--- assigment 4/part 4/main-finished.js
+++ assigment 4/part 4/main-finished.js
@@ -81,6 +81,74 @@ class Ball extends Shape {
     }
   }
 }
+class EvilCircle extends Shape{
+  constructor(x,y){
+
+    super(x,y,20,20);
+    this.color = 'white';
+    this.size = 10;
+    window.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+  
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--;
+            element.innerHTML = 'Ball count: ' + count;
+          }
+        }
+      }
+    }
+}
 
 
 
@@ -101,6 +169,10 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
+let evilcircle = new EvilCircle(
+  random(0,width),
+  random(0,height)
+)
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
