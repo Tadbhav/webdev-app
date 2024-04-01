fa01404fc1a0413556dbf8da84fe50299d0fea32
Tadbhav <tadbhav.marken@dcmail.ca>
2024-04-01 18:15:16 -0400

    declaring alternative

--- assigment 4/part 2/main.js
+++ assigment 4/part 2/main.js
@@ -7,7 +7,7 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const imageArray =['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 /* Declaring the alternative text for each image file */
-
+const altText =['pic1','pic2','pic3','pic4','pic5'];
 /* Looping through images */
 
 const newImage = document.createElement('img');
