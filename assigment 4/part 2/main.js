3544dcd072d7e3b8167f62a2e3687d546def2562
Tadbhav <tadbhav.marken@dcmail.ca>
2024-04-01 18:18:31 -0400

    looping through images

--- assigment 4/part 2/main.js
+++ assigment 4/part 2/main.js
@@ -9,6 +9,17 @@ const imageArray =['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 /* Declaring the alternative text for each image file */
 const altText =['pic1','pic2','pic3','pic4','pic5'];
 /* Looping through images */
+for( let i = 0; i < imageArray.length; i++ )
+{
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + imageArray[i]);
+    newImage.setAttribute('alt', alternativeTextArray[i]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', (e) => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
